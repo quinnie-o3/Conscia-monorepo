c8e6cc333a611c3b333bb2b33b35745d35896ff8
NGUYEN THI QUYNH NHI <23521111@gm.uit.edu.vn>
2026-05-12 09:32:53 +0700

    Delete usage-tracker-backend directory

--- usage-tracker-backend/src/modules/purpose-tag/purpose-tag.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PurposeTagController } from './purpose-tag.controller';
-import { PurposeTagService } from './purpose-tag.service';
-
-@Module({
-  controllers: [PurposeTagController],
-  providers: [PurposeTagService]
-})
-export class PurposeTagModule {}
